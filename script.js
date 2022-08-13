369e2638d75d30eba8ff8592a9cbba2e77fb8523
Daniel <ricardotavera64@gmail.com>
2022-08-12 23:03:13 -0500

    update

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 
 
-function add(){
-    
-    addInTable(text)
-}
 
 function addInTable() {
-    let input = document.getElementById('add-button')
-    var table = document.getElementById("table");
-    var rows = document.getElementsByTagName('tr')
-    var newRow = table.insertRow(0);
-    var col2 = newRow.insertCell(1);
-    var col3 = newRow.insertCell(2);
-    var today = new Date();
+  let input = document.getElementById('weather')
 
-    col2 = input.value;
-    col3 = today.toLocaleDateString() +' '+ today.toLocaleTimeString();
+ if(input.value){
+  let table = document.getElementById("table-body");
+  let newRow = table.insertRow(0);
+  let col1 = newRow.insertCell(0)
+  let col2 = newRow.insertCell(1);
+  let col3 = newRow.insertCell(2);
+  let today = new Date();
 
-    text = 'textContent' in document ? 'textContent' : 'innerText';
 
-    for (var i = 0, len = rows.length; i < len; i++) {
-  rows[i].children[0][text] = i + ': ' + rows[i].children[0][text];
+  col2.innerHTML = input.value;
+  col3.innerHTML = today.toLocaleDateString().replace(/[/]/g,'-') + ' ' + today.toLocaleTimeString();
 
+  let rows = document.getElementsByTagName('tr')
+  text = 'textContent' in document ? 'textContent' : 'innerText';
+
+  for (let i = 0, len = rows.length; i < len; i++) {
+    rows[i+1].children[0][text] = i+1;
   }
+   
+ }
+
+ /* input.value = '' */
+}
+
+function deleteRecord() {
+  
+  let table = document.getElementById("table-body");
+  
+    if( table.rows.length > 0){
+      table.deleteRow(0);
+    }
+   
+  
+
+}
 
 
+
+function change() // no ';' here
+{
+  var elem = document.getElementById("toogle");
+    if (elem.value=="Cronometro") {
+      document.getElementById('wetab').style.display = 'flex'
+      elem.value = "Tabla"}
+    else{ elem.value = "Cronometro";
+    document.getElementById('wetab').style.display = 'none'
+    
+  }
 }
